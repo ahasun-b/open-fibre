9f56243d65bf30477b79cdef9d65c746f5e89852
ahasun-b <ahasun.bdcalling@gmail.com>
2023-09-18 17:01:57 +0600

    ENV added

--- src/pages/api/appointment.js
+++ src/pages/api/appointment.js
@@ -4,42 +4,43 @@ import Cors from 'cors';
 import axios from 'axios';
 
 const cors = Cors({
-  methods: ['POST', 'GET', 'HEAD'],
+    methods: ['POST', 'GET', 'HEAD'],
+    origin: 'https://open-fibre.co.uk', // Allow requests from this origin
 });
 
 function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result);
+            }
 
-      return resolve(result);
+            return resolve(result);
+        });
     });
-  });
 }
 
 export default async function handler(req, res) {
-  await runMiddleware(req, res, cors);
-  if (req.method !== 'POST') return res.status(200).json({ name: 'Hi Franky' });
+    await runMiddleware(req, res, cors);
+    if (req.method !== 'POST') return res.status(200).json({ name: 'Hi Franky' });
 
-  const bodyData = req.body;
-  const authorization = req.headers.authorization;
+    const bodyData = req.body;
+    const authorization = req.headers.authorization;
 
-  try {
-    const resp = await axios.get(
-      'https://api.ms3.net/api/V1/Appointments/GetAvailableAppointments',
-      {
-        headers: {
-          Authorization: authorization,
-          'Content-Type': 'application/json',
-        },
-        data: JSON.stringify(bodyData),
-      }
-    );
+    try {
+        const resp = await axios.get(
+            'https://api.ms3.net/api/V1/Appointments/GetAvailableAppointments',
+            {
+                headers: {
+                    Authorization: authorization,
+                    'Content-Type': 'application/json',
+                },
+                data: JSON.stringify(bodyData),
+            }
+        );
 
-    res.status(200).json(resp.data);
-  } catch (error) {
-    res.status(400).json(error);
-  }
+        res.status(200).json(resp.data);
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
