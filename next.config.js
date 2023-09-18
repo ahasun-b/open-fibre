dc216a08384f32d3c430dd251d34ea9a7469ed62
ahasun-b <ahasun.bdcalling@gmail.com>
2023-09-18 17:09:32 +0600

    time out code update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    // Increase the timeout to 30 seconds (or any suitable value)
+   timeout: 30,
+
 }
 
 module.exports = nextConfig
